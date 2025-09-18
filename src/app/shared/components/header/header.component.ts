1f966a8a6c4c2bd5039d8d86b51c2ecebd861413
lasgra <tomekoclon1@gmail.com>
2025-09-18 16:29:57 +0200

    navigation changes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,19 +1,43 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, HostListener, inject, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {AccountDto} from "../../utility/dtos/AccountDto";
 import {AuthService} from "../../../services/api/account/auth.service";
 import {AccountService} from "../../../services/api/account/account.service";
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'pp-header',
   templateUrl: './header.component.html',
+  animations: [
+    trigger('animate', [
+      state('hidden', style({
+        top: "-56px"
+      })),
+      state('visible', style({
+        top: 0
+      })),
+      transition('hidden <=> visible', [
+        animate('0.5s ease')
+      ])
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
-
   private authService = inject(AuthService);
   private accountService = inject(AccountService);
-
+  lastScrollTop = 0;
+  headershown = "visible";
   account$: Observable<AccountDto> | undefined;
+
+
+  @HostListener('window:scroll', ['$event']) onWindowScroll(e) {
+    const scrollTop = e.target['scrollingElement'].scrollTop;
+
+    this.lastScrollTop - scrollTop < 0 ? this.headershown = "hidden" : this.headershown = "visible";
+    console.log(this.headershown);
+    
+    this.lastScrollTop = scrollTop;    
+  };
 
   onLogoClick() {
     document.documentElement.scrollTop = 0;
